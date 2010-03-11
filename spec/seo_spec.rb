9d7a18b1ad70d16a4b641cd4b82c7f04ebc09060
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-03-11 20:02:35 +0100

    Written the test scenarios in which the extension is used properly.

--- spec/seo_spec.rb
+++ spec/seo_spec.rb
@@ -9,12 +9,52 @@ describe Sinatra::Seo do
   end
   
   context "when initialized" do
-    it "the :seo_file option should be null." do
+    before :all do
+      set :seo_file, nil
+    end
+    
+    it "then the :seo_file option should be null." do
       app.seo_file.should be_nil
     end
     
-    it "the seo helper should release an error." do
+    it "then the seo helper method should release an error." do
       lambda{seo}.should raise_error(TypeError)
+    end
+  end
+  
+  context "when defined an existent SEO file" do
+    before :all do
+      File.open('./test.seo', 'w'){|file| file.write(YAML::dump(DATA))}
+      
+      set :seo_file, './test.seo'
+    end
+    
+    it "then the :seo_file option should contain the path to the SEO file." do
+      File.exist?('./test.seo').should be_true
+      File.extname(app.seo_file).should == '.seo'
+      
+      app.seo_file.should_not be_nil
+      app.seo_file.should be_an_instance_of(String)
+      app.seo_file.should == './test.seo'
+    end
+    
+    it "then the seo helper method should include every page (with :title, :description and :keywords as sub-attributes) as a read-only attribute." do
+      YAML.load_file('./test.seo').keys.each do |page|
+        seo.methods.include?(page).should be_true
+        seo.methods.include?(:"#{page}=").should be_false
+        
+        eval("seo.#{page}").should be_an_instance_of(OpenStruct)
+        
+        [:title, :description, :keywords].each do |atribute|
+          eval("seo.#{page}.methods").include?(attribute).should be_true
+          eval("seo.#{page}.methods").include?(:"#{attribute}=").should be_false
+          eval("seo.#{page}.#{attribute}").should be_an_instance_of(String)
+        end
+      end
+    end
+    
+    after :all do
+      File.delete './test.seo'
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,7 @@ require File.join(Dir.pwd, %w{lib sinatra seo})
 
 set :environment, :test
 enable :raise_errors
+
+DATA = {:test1 => {:title => 'Title1', :description => 'Description1', :keywords => 'Keywords1'}, 
+        :test2 => {:title => 'Title2', :description => 'Description2', :keywords => 'Keywords2'}, 
+        :test3 => {:title => 'Title3', :description => 'Description3', :keywords => 'Keywords3'}}
