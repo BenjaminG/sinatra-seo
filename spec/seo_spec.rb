5558630926f60e2182772bf882907aa760d3b4d3
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-03-11 16:28:40 +0100

    Written the test scenarios in which the extension is used as default.

--- spec/seo_spec.rb
+++ spec/seo_spec.rb
@@ -2,8 +2,19 @@ require 'spec_helper'
 
 describe Sinatra::Seo do
   include Rack::Test::Methods
+  include Sinatra::Seo::Helpers
   
   def app
     Sinatra::Application
+  end
+  
+  context "when initialized" do
+    it "the :seo_file option should be null." do
+      app.seo_file.should be_nil
+    end
+    
+    it "the seo helper should release an error." do
+      lambda{seo}.should raise_error(TypeError)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,4 @@ require 'rack/test'
 require File.join(Dir.pwd, %w{lib sinatra seo})
 
 set :environment, :test
+enable :raise_errors
