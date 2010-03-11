66f73e7edd3e0e3c8d4fe287219b1e052bec6df9
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-03-11 16:46:02 +0100

    Defined the default :seo_file option and created the basic Sinatra::Seo::Helpers#seo method.

--- lib/sinatra/seo.rb
+++ lib/sinatra/seo.rb
@@ -1,7 +1,22 @@
 require 'sinatra/base'
+require 'yaml'
 
 module Sinatra
   module Seo
-    # ...
+    module Helpers
+      def seo
+        @seo ||= begin
+          @seo = YAML.parse_file(app.seo_file)
+        end
+      end
+    end
+    
+    def self.registered(app)
+      app.helpers Seo::Helpers
+      
+      app.set :seo_file, nil
+    end
   end
+  
+  register Seo
 end
