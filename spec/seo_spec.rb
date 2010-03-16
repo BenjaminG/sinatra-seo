7f6179a563915524de53466c9b268e4dfe5984a0
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-03-16 16:50:43 +0100

    Written the test scenarios in which the size of the sub-attributes is measured.

--- spec/seo_spec.rb
+++ spec/seo_spec.rb
@@ -29,33 +29,55 @@ describe Sinatra::Seo do
       set :seo_file, './test.seo'
     end
     
-    it "then the :seo_file option should contain the path to the SEO file." do
-      app.seo_file.should_not be_nil
-      app.seo_file.should be_an_instance_of(String)
-      app.seo_file.should == './test.seo'
-    end
-    
-    it "then the defined :seo_file should exist and have the '.seo' extension." do
-      File.exist?('./test.seo').should be_true
-      File.extname(app.seo_file).should == '.seo'
-    end
-    
-    it "then the seo helper method should include every page (with :title, :description and :keywords as sub-attributes) as a read-only attribute." do
-      YAML.load_file('./test.seo').keys.each do |page|
-        seo.methods.include?(page).should be_true
-        seo.methods.include?(:"#{page}=").should be_false
-        
-        eval("seo.#{page}").should be_an_instance_of(OpenStruct)
-        
-        [:title, :description, :keywords].each do |attribute|
-          eval("seo.#{page}.methods").include?(attribute).should be_true
-          eval("seo.#{page}.methods").include?(:"#{attribute}=").should be_false
-          eval("seo.#{page}.#{attribute}").should be_an_instance_of(String)
-          eval("seo.#{page}.#{attribute}").should == DATA[page][attribute]
-        end
+    context "then the :seo_file" do
+      it "should contain the path to the SEO file." do
+        app.seo_file.should_not be_nil
+        app.seo_file.should be_an_instance_of(String)
+        app.seo_file.should == './test.seo'
+      end
+
+      it "should exist and have the '.seo' extension." do
+        File.exist?('./test.seo').should be_true
+        File.extname(app.seo_file).should == '.seo'
       end
     end
     
+    context "then the seo helper method" do
+      it "should include every page with its sub-attributes as a read-only attributes." do
+        YAML.load_file('./test.seo').keys.each do |page|
+          seo.methods.include?(page).should be_true
+          seo.methods.include?(:"#{page}=").should be_false
+
+          eval("seo.#{page}").should be_an_instance_of(OpenStruct)
+
+          [:title, :description, :keywords].each do |attribute|
+            eval("seo.#{page}.methods").include?(attribute).should be_true
+            eval("seo.#{page}.methods").include?(:"#{attribute}=").should be_false
+            eval("seo.#{page}.#{attribute}").should be_an_instance_of(String)
+            eval("seo.#{page}.#{attribute}").should == DATA[page][attribute]
+          end
+        end
+      end
+      
+      it "should allow a maximum of 60 characters for the Title sub-attribute." do
+        seo.instance_variable_get(:@table).keys.each do |page|
+          eval("seo.#{page}.title").size.should <= 60
+        end
+      end
+      
+      it "should allow a maximum of 160 characters for the Description aub-attribute." do
+        seo.instance_variable_get(:@table).keys.each do |page|
+          eval("seo.#{page}.description").size.should <= 160
+        end
+      end
+      
+      it "should allow a maximum of 15 words for the Keywords aub-attribute." do
+        seo.instance_variable_get(:@table).keys.each do |page|
+          eval("seo.#{page}.keywords").split(" ").size.should <= 15
+        end
+      end
+    end
+
     after :all do
       File.delete './test.seo'
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,13 @@ require File.join(Dir.pwd, %w{lib sinatra seo})
 set :environment, :test
 enable :raise_errors
 
-DATA = {:test1 => {:title => 'Title1', :description => 'Description1', :keywords => 'Keywords1'}, 
-        :test2 => {:title => 'Title2', :description => 'Description2', :keywords => 'Keywords2'}, 
-        :test3 => {:title => 'Title3', :description => 'Description3', :keywords => 'Keywords3'}}
+DATA = {:test1 => { :title => lambda{word = ""; 50.times{word += "t"}; word}.call, 
+                    :description => lambda{word = ""; 150.times{word += "d"}; word}.call, 
+                    :keywords => lambda{word = ""; 10.times{word += "k, "}; word}.call }, 
+        :test2 => { :title => lambda{word = ""; 60.times{word += "t"}; word}.call, 
+                    :description => lambda{word = ""; 160.times{word += "d"}; word}.call, 
+                    :keywords => lambda{word = ""; 15.times{word += "k, "}; word}.call }, 
+        :test3 => { :title => lambda{word = ""; 70.times{word += "t"}; word}.call, 
+                    :description => lambda{word = ""; 170.times{word += "d"}; word}.call, 
+                    :keywords => lambda{word = ""; 20.times{word += "k, "}; word}.call }}
+                    
