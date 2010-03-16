ddb144be0dbb4c3cc928c5dd178cae0395f2f286
Julio Javier Cicchelli <javier@rock-n-code.com>
2010-03-16 17:30:54 +0100

    Added the measuring of the sub-attributes of every page.

--- lib/sinatra/seo.rb
+++ lib/sinatra/seo.rb
@@ -6,6 +6,13 @@ class OpenStruct
   def new_ostruct_member(name)
     name = name.to_sym
     
+    @table[name] = case name
+      when :title then @table[name][0, 60]
+      when :description then @table[name][0, 160]
+      when :keywords then @table[name].split(" ")[0, 15].join(" ")
+      else @table[name]
+    end
+    
     unless self.respond_to?(name)
       class << self; self; end.class_eval do
         define_method(name) {@table[name].is_a?(Hash) ? OpenStruct.new(@table[name]) : @table[name]}
